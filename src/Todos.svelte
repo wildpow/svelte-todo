cdf9bfd0c3634e8817c4f48db83c7a57edf149ed
wildpow <element1028@gmail.com>
2020-03-20 01:33:06 -0700

    building out components

---
+++ src/Todos.svelte
@@ -0,0 +1,77 @@
+<script>
+  import TodoItem from "./TodoItem";
+
+  let newTodoTitle = "";
+  let currentFilter = "all";
+  let next = 4;
+
+  let todos = [
+    {
+      id: 1,
+      title: "My first todo",
+      completed: false
+    },
+    {
+      id: 2,
+      title: "My second todo",
+      completed: false
+    },
+    {
+      id: 3,
+      title: "My third todo",
+      completed: false
+    }
+  ];
+</script>
+
+<div class="container">
+  <h1>Svelte Todos</h1>
+  <input
+    type="text"
+    class="todo-input"
+    placeholder="Insert todo item..."
+    bind:value={newTodoTitle}
+    on:keydown={addTodo} />
+
+  {#each filteredTodos as todo}
+    <div class="todo-item">
+      <TodoItem
+        {...todo}
+        on:deleteTodo={handleDeleteTodo}
+        on:toggleComplete={handleToggleComplete} />
+    </div>
+  {/each}
+
+  <div class="inner-container">
+    <div>
+      <label for="">
+        <input
+          type="checkbox"
+          class="inner-container-input"
+          on:change={checkAllTodos} />
+        Check All
+      </label>
+    </div>
+    <div>{todosRemaining} items left</div>
+  </div>
+
+  <div class="inner-container">
+    <div>
+      <button
+        on:click={() => updateFilter('all')}
+        class:active={currentFilter === 'all'}>
+        All
+      </button>
+      <button
+        on:click={() => updateFilter('active')}
+        class:active={currentFilter === 'active'}>
+        Active
+      </button>
+      <button
+        on:click={() => updateFilter('completed')}
+        class:active={currentFilter === 'completed'}>
+        completed
+      </button>
+    </div>
+  </div>
+</div>
