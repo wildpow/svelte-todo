9a8b22c29d8f10a1eac1b0693a24580a5633bf97
wildpow <element1028@gmail.com>
2020-03-20 11:54:45 -0700

    WIP Todos component

--- src/Todos.svelte
+++ src/Todos.svelte
@@ -38,10 +38,79 @@
       : currentFilter === "completed"
       ? todos.filter(todo => todo.completed)
       : todos.filter(todos => !todo.completed);
+
+  function checkAllTodos(event) {
+    todos.forEach(todo => (todo.completed = event.target.checked));
+    todos = todos;
+  }
+
+  function updateFilter(newFilter) {
+    currentFilter = newFilter;
+  }
+
+  function clearCompleted(event) {
+    todos = todos.filter(todo => !todo.completed);
+  }
+
+  function handleDeleteTodo(event) {
+    todos = todos.filter(todo => todo.id !== event.detail.id);
+  }
+
+  function handleToggleComplete(event) {
+    const todoIndex = todos.findIndex(todo => todo.id === event.detail.id);
+    const updatedTodo = {
+      ...todos[todoIndex],
+      completed: !todos[todoIndex].completed
+    };
+
+    todos = [
+      ...todos.slice(0, todoIndex),
+      updatedTodo,
+      ...todos.slice(todoIndex + 1)
+    ];
+  }
 </script>
 
+<style>
+  .container {
+    max-width: 800px;
+    margin: 10px auto;
+  }
+  .logo {
+    display: block;
+    margin: 20px auto;
+    width: 50%;
+  }
+  .todo-input {
+    width: 100%;
+    padding: 10px 20px;
+    font-size: 18px;
+    margin-bottom: 20px;
+  }
+  .inner-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 16px;
+    border-top: 1px solid lightgray;
+    padding-top: 15px;
+    margin-bottom: 13px;
+  }
+  .inner-container-input {
+    margin-right: 12px;
+  }
+  button {
+    font-size: 14px;
+    background-color: white;
+    appearance: none;
+  }
+  button:hover {
+    background: lightseagreen;
+  }
+</style>
+
 <div class="container">
-  <h1>Svelte Todos</h1>
+  <h1 class="logo">Svelte Todos</h1>
   <input
     type="text"
     class="todo-input"
